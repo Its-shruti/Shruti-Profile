ddea38d40537c975b9f514f117425d02dafc36e2
shruti kumari <104879589+Its-shruti@users.noreply.github.com>
2024-09-17 12:01:34 +0530

    data folder added

---
+++ jsonData/project.js
@@ -0,0 +1,90 @@
+export const arrData = [
+    {
+        "title": "News App",
+        "detail": "Dynamic news app using React, integrating with a public news API to fetch the latest headlines and updates from various sources. The application allows users to browse through different categories such as technology, sports, entertainment, and more.",
+        "imgPath": "images/galaxyAir.PNG",
+        "technology": "React, JavaScript, CSS",
+        "link": "https://galaxyair.netlify.app/",
+        "alt": "image"
+    },
+    {
+        "title": "Recipe Finder",
+        "detail": "This is a dynamic recipe finder application built using React. Users can search for recipes by ingredient or dish name and receive a list of relevant recipes. The app features an intuitive UI for easy navigation and interaction",
+        "imgPath": "images/foodSangam.PNG",
+        "technology": "React, JavaScript, CSS",
+        "link": "https://foodsangam.netlify.app/",
+        "alt": "image"
+    },
+    {
+        "title": "Text Converter",
+        "detail": "Text Converter Web App with features like converting text to uppercase, lowercase, title-case, retrieving the meaning of single words using an API, word count, character count, and theme-changing functionality",
+        "imgPath": "images/textConverter.PNG",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-Text-Converter/",
+        "alt": "image"
+    },
+    {
+        "title": "Gemini Clone",
+        "detail": "Integrated with a cryptocurrency API (e.g., CoinGecko, CoinMarketCap) to fetch and display live result.  Incorporated a theme switching feature to allow users to toggle between light and dark modes",
+        "imgPath": "images/geminiClone.PNG",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-Gemini-Clone/",
+        "alt": "image"
+    },
+    {
+        "title": "Gradient Changer",
+        "detail": "It allows the user to change the background gradient dynamically, Using JavaScript, you can change the background gradient based on user input, such as selecting colors or clicking button",
+        "imgPath": "images/GradientChanger.PNG",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-Gradient-Changer/",
+        "alt": "image"
+    },
+    {
+        "title": "Image Slider",
+        "detail": "Autoplay Settings: Adjust the interval in the setInterval() function to change the autoplay speed. You can add more slides",
+        "imgPath": "images/imageSlider.PNG",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-Image-Slider/",
+        "alt": "image"
+    },
+    {
+        "title": "ToDo List",
+        "detail": "A To-Do List is a simple project that demonstrates core JavaScript skills like DOM manipulation and working with localStorage, Users can input new tasks through a form, which will be added to the list",
+        "imgPath": "images/toDoList.PNG ",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-ToDo-List/",
+        "alt": "image"
+    },
+    {
+        "title": "Weather App",
+        "detail": "A responsive weather application built using JavaScript that fetches real-time weather data from a weather API. App shows current weather conditions by city and displays temperature, weather conditions, wind speed, and humidity",
+        "imgPath": "images/weatherApp.PNG",
+        "technology": "HTML, CSS, JavaScript",
+        "link": "https://its-shruti.github.io/JS-weatherApp/",
+        "alt": "image"
+    },
+    {
+        "title": "Template 1",
+        "detail": "Basic E-commerce template for CSS practice, including animations",
+        "imgPath": "images/template1.PNG",
+        "technology": "HTML, CSS",
+        "link": "https://its-shruti.github.io/CSS-Template1/",
+        "alt": "image"
+    },
+    {
+        "title": "Template 2",
+        "detail": "Basic template for CSS practice, including animations",
+        "imgPath": "images/template2.PNG",
+        "technology": "HTML, CSS",
+        "link": "https://its-shruti.github.io/CSS-Template2/",
+        "alt": "image"
+    },
+    {
+        "title": "Template 3",
+        "detail": "Basic E-commerce template for CSS practice, including animations",
+        "imgPath": "images/template3.PNG",
+        "technology": "HTML, CSS",
+        "link": "https://its-shruti.github.io/CSS-Template3/",
+        "alt": "image"
+    }
+]
