f5f230e557787cc4f6bae9c0852aa2fe54df4639
shruti kumari <104879589+Its-shruti@users.noreply.github.com>
2025-01-04 11:49:46 +0530

    rest files

---
+++ jsonData/skills.js
@@ -0,0 +1,27 @@
+export const skillData = [
+    {
+        skill: "HTML",
+        icon_class: "fa-solid fa-code",
+        detail: "HTML is the standard markup language used to create and structure content on the web, defining elements like headings, paragraphs, links, images, and other media."
+    },
+    {
+        skill: "CSS",
+        icon_class: "fa-brands fa-css3",
+        detail: "CSS is a style sheet language used to describe the presentation of a document written in HTML or XML, controlling layout, colors, fonts, and spacing to enhance visual appearance."
+    },
+    {
+        skill: "JS",
+        icon_class: "fa-brands fa-square-js",
+        detail: "JavaScript is a versatile, high-level scripting language primarily used for adding interactivity to web pages, enabling dynamic content, and building web appthrough its integration with HTML."
+    },
+    {
+        skill: "React",
+        icon_class: "fa-brands fa-react",
+        detail: "React is a popular JavaScript library for building user interfaces, known for its component-based architecture, reusability, and efficiency in managing dynamic data through a virtual DOM."
+    },
+    {
+        skill: "JAVA",
+        icon_class: "fa-brands fa-java",
+        detail: "Java is a versatile, object-oriented programming language widely used for building robust applications. It supports platform independence, making it ideal for web, mobile, and enterprise solutions."
+    }
+]
